fdee41c1cef058c61a963d4aaac7996894a143fc
Garrett Credi [ddxtanx] <gcc@ameritech.net>
2018-10-21 16:58:22 -0500

    1.5.3

--- skyward_api/skyward_class.py
+++ skyward_api/skyward_class.py
@@ -26,3 +26,21 @@ class SkywardClass():
         grades_str = '\n'.join(text_grades_tabbed)
 
         return "{0}:\n {1}".format(self.name, grades_str)
+
+    def __sub__(self, other: object) -> "SkywardClass":
+        if not isinstance(other, SkywardClass):
+            raise ValueError("- only defined between SkywardClass object.")
+        if self.name != other.name:
+            raise ValueError(
+                (
+                    "- only defined between the same SkywardClasses. Your "
+                    " two classes have different names."
+                )
+            )
+        my_grades = self.grades
+        their_grades = other.grades
+        diff_grades = [
+            grade
+            for grade in my_grades if grade not in their_grades
+        ]
+        return SkywardClass(self.name, diff_grades)
